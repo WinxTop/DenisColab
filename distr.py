5ce72ec3a4a399c25418f6626ea3462a9df7357c
Kirill <kir.karasyov2017@yandex.ru>
2021-10-24 23:03:15 +0300

    Added plotting

---
+++ distr.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def main():
+    x_max = 3
+    y_max = 3
+    coords = read_file()
+    for i in coords:
+        print(i)
+    square = rad_calc(coords, x_max, y_max)
+    #print(square[0].shape)
+    for x in range(0, x_max):
+        for y in range(0, y_max):
+            print(square[x][y], end = " ")
+        print(end = "\n")
+
+def read_file(file_name="input.txt"):
+    coords = list()
+    for line in open(file_name):
+        line = line.rstrip('\n')
+        vals = line.split(" ")
+        coords.append(np.array([float(vals[0]), float(vals[1])]))
+    return coords
+
+def rad_calc(coords, x_max, y_max):
+    square = np.zeros((x_max, y_max))
+    for x in range(0, x_max):
+        for y in range(0, y_max):
+            r = list()
+            for sprayer in coords:
+                d_x = (x + 1) - sprayer[0]
+                d_y = (y + 1) - sprayer[1]
+                r.append(np.sqrt(d_x**2+d_y**2))
+            for k in r:
+                intensity = 1/(np.sqrt(1+(k)**2))
+                square[x][y] += intensity
+    return square
+
+if __name__ == "__main__":
+    main()
