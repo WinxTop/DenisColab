6b520ced26465500c3e4afc60d9aad1e70ce7b50
Kirill <kir.karasyov2017@yandex.ru>
2021-10-28 23:26:50 +0300

    Added files to repository

--- distr_plot.py
+++ distr_plot.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def main():
-    x_max = 3
-    y_max = 3
+    x_max = 6
+    y_max = 6
     coords = read_file()
     for i in coords:
         print(i)
@@ -13,14 +13,16 @@ def main():
         for y in range(0, y_max):
             print(square[x][y], end = " ")
         print(end = "\n")
+    data = square.reshape(square.shape[0]*square.shape[1])
     plotting(square, x_max, y_max)
+    histogram(data)
 
 def read_file(file_name="input.txt"):
     coords = list()
     for line in open(file_name):
         line = line.rstrip('\n')
         vals = line.split(" ")
-        coords.append(np.array([float(vals[0]), float(vals[1])]))
+        coords.append(np.array([float(vals[0]), float(vals[1]), float(vals[2])]))
     return coords
 
 def rad_calc(coords, x_max, y_max):
@@ -31,9 +33,9 @@ def rad_calc(coords, x_max, y_max):
             for sprayer in coords:
                 d_x = (x + 1) - sprayer[0]
                 d_y = (y + 1) - sprayer[1]
-                r.append(np.sqrt(d_x**2+d_y**2))
-            for k in r:
-                intensity = 1/(np.sqrt(1+(k)**2))
+                r.append(d_x**2+d_y**2)
+            for i in range(0, len(r)):
+                intensity = coords[i][2]/(1+(r[i]))
                 square[x][y] += intensity
     return square
 
@@ -43,8 +45,29 @@ def plotting(square, x_max, y_max):
 
     fig, ax = plt.subplots()
     ax.pcolormesh(x, y, square)
-    plt.show()
     ax.set_aspect('equal')
+    plt.pcolor(square, cmap = plt.get_cmap('viridis', 11))
+    plt.colorbar()
+    plt.show()
+
+def compute_histogram_bins(data, desired_bin_size):
+    min_val = np.min(data)
+    max_val = np.max(data)
+    min_boundary = 0
+    max_boundary = max_val - max_val % desired_bin_size + desired_bin_size
+    n_bins = int((max_boundary - min_boundary) / desired_bin_size) + 1
+    num_bins = np.linspace(min_boundary, max_boundary, n_bins)
+    return num_bins
+
+def histogram(data):
+    desired_bin_size = 0.1
+    num_bins = compute_histogram_bins(data, desired_bin_size)
+    n, bins, patches = plt.hist(data, num_bins, facecolor='green', alpha=1)
+    plt.xlabel('intensity')
+    plt.ylabel('count')
+    plt.title('intensity distribution')
+    plt.grid(True)
+    plt.show()
 
 if __name__ == "__main__":
     main()
